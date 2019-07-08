cf2c20c25f1c5f8049c8f471a404e47002f77cdd
Dhanu K Parvateesam <bewithdhanu@gmail.com>
2019-07-08 13:43:28 +0530

    Update foldable.js

--- foldable.js
+++ foldable.js
@@ -1,63 +1,68 @@
 "use strict";
-var table = null,
-n=null,
-expanded=[];
 $.fn.foldableTable = function (t) {
-    n = {
+    var table = this;
+    var expanded=[];
+    var params = $.extend({
         fieldName : null,
         filter: false,
         filter_row:1,
         rowIndex:false,
-        collapsableHead:true,
+        multiPaste:false,
+        pasteInCollapse:true,
+        collapsableHead:false,
+        defaultCollapse:false,
         filter_except:[],
         methodName: function () {}
-    };
-    var params = $.extend(n, t); 
-    table = this;
-    initFoldableTable(params);
-    function initFoldableTable(params){
-        if(params.filter){
-            var hrows = table.find("thead tr").length;
-            if(params.filter_row>0 && hrows>=params.filter_row){
-                table.find("thead tr:eq("+(params.filter_row-1)+") th").each(function(index){
-                    if($.inArray(index, params.filter_except)){
-                        $(this).append("<select class='selectpicker'></select>")
-                    }
-                });
-            }else{
-                console.error("filter_row:"+params.filter_row+" is not available")
-            }
-        }
-        if(params.rowIndex){
-            var hrows = table.find("thead tr th").length;
-            table.find("thead tr").each(function(index){
-                if(hrows-1==index){
-                    $(this).prepend("<th></th>");
-                }else{
-                    $(this).prepend("<th>#</th>");
+    }, t); 
+    
+    if(params.filter){
+        var hrows = table.find("thead tr").length;
+        if(params.filter_row>0 && hrows>=params.filter_row){
+            table.find("thead tr:eq("+(params.filter_row-1)+") th").each(function(index){
+                if($.inArray(index, params.filter_except)){
+                    $(this).append("<select class='selectpicker'></select>")
                 }
             });
-            table.find("tbody tr").each(function(index){
-                $(this).prepend("<td>"+(index+1)+"</td>");
-            });
-        }
-        if(params.multiPaste){
-            enableMultiPase();
-        }
-        if(params.collapsableHead){
-            var ocols=0;
-            table.find("thead tr td,thead tr th").each(function(index){
-                if($(this).hasAttr("colspan")){
-                    $(this).prepend("<span style='border: 1px solid #636363;background: #797979;color: #fff;border-radius: 3px;padding: 0px 4px;cursor: pointer;margin-right: 5px;font-weight: bold;' data-index='"+index+"' class='collapsableHead' id='collapsableHead"+index+"' data-collapse='on' data-ocols='"+ocols+"' data-colspan='"+$(this).attr("colspan")+"'>-</span>");
-                    enableCollapsableHead($("#collapsableHead"+index));
-                    ocols += parseInt($(this).attr("colspan"));
-                }else{
-                    ocols += 1;
-                }
-            });
+        }else{
+            console.error("filter_row:"+params.filter_row+" is not available")
         }
     }
-    $.fn.addRow = function(cols) {
+    if(params.rowIndex){
+        var hrows = table.find("thead tr th").length;
+        table.find("thead tr").each(function(index){
+            if(hrows-1==index){
+                $(this).prepend("<th></th>");
+            }else{
+                $(this).prepend("<th>#</th>");
+            }
+        });
+        table.find("tbody tr").each(function(index){
+            $(this).prepend("<td>"+(index+1)+"</td>");
+        });
+    }
+    if(params.multiPaste){
+        enableMultiPaste(table); 
+    }
+    if(params.collapsableHead){
+        var ocols=0;
+        table.find("thead tr td,thead tr th").each(function(index){
+            if($(this).hasAttr("colspan")){
+                $(this).prepend("<span style='border: 1px solid #636363;background: #797979;color: #fff;border-radius: 3px;padding: 0px 4px;cursor: pointer;margin-right: 5px;font-weight: bold;' data-index='"+index+"' class='collapsableHead' id='collapsableHead"+index+"' data-collapse='on' data-ocols='"+ocols+"' data-colspan='"+$(this).attr("colspan")+"'>-</span>");
+                enableCollapsableHead(table.find("#collapsableHead"+index),table);
+                ocols += parseInt($(this).attr("colspan"));
+            }else{
+                ocols += 1;
+            }
+        });
+    }
+    if(params.defaultCollapse){
+        table.find(".collapsableHead").each(function(){
+            if($(this).data("collapse")=="on"){ 
+                $(this).click();
+            }
+        });
+    }
+    this.addRow = function(cols,callback) {
         var content = "<tr>";
         if(params.rowIndex){
             var rows = table.find("tbody tr").length;
@@ -68,17 +73,29 @@ $.fn.foldableTable = function (t) {
         });
         content += "</tr>";
         if(params.collapsableHead){
-            table.expandAllColumns();
+            this.expandAllColumns();
         }
-        table.find("tbody").append(content);
+        var tbody = table.find("tbody").append(content);
+        if(typeof callback !== 'undefined'){
+            callback.complete(tbody.find("tr:last"));
+        }
         if(params.multiPaste){
-            enableMultiPase();
+            enableMultiPaste(table);
         }
         if(params.collapsableHead){
-            table.revertAllColumns();
+            this.revertAllColumns();
         }
     };
-    $.fn.deleteRow = function(index){
+    this.copyRow = function(callback) {
+        var cols = [];
+        table.find("tbody tr:eq(0) td").each(function(index){
+            if((params.rowIndex && index>0) || !params.rowIndex){
+                cols.push($(this).html());
+            }
+        });
+        this.addRow(cols,callback);
+    };
+    this.deleteRow = function(index){
         table.find("tbody tr").each(function(i){
             if(i==index){
                 $(this).remove();
@@ -90,23 +107,22 @@ $.fn.foldableTable = function (t) {
             });
         }
     };
-    $.fn.revertAllColumns = function(){
+    this.revertAllColumns = function(){
         $.each(expanded,function(ind,ele){
-            $("#"+ele).click();
+            table.find("#"+ele).click();
         });
         expanded = [];
     };
-    $.fn.expandAllColumns = function(){
+    this.expandAllColumns = function(){
         table.find(".collapsableHead").each(function(){
-            if($(this).data("collapse")=="off"){
+            if($(this).data("collapse")=="off"){ 
                 $(this).click();
                 expanded.push($(this).attr("id"));
             }
-        })
+        });
     };
-
-    function enableMultiPase(){
-        table.find('input').bind('paste', null,null);
+    function enableMultiPaste(table){
+        table.find('input').unbind('paste');
         table.find('input').bind('paste', null, function (e) {
             var ele = $(this);
             setTimeout(function(){
@@ -114,23 +130,37 @@ $.fn.foldableTable = function (t) {
                 var currentRowIndex = ele.closest("tr").index();
                 var tbody = ele.closest("tbody");
                 var row = clipboardData.split("\n");
-                tbody.find("tr").each(function(i){
-                    if(i>=currentRowIndex){
-                        if(typeof row[i-currentRowIndex]  !== 'undefined' && !row[i-currentRowIndex].isEmpty()){
-                            var columns = row[i-currentRowIndex].split(/\s+/);
-                            $(this).find("td input").each(function(j){
-                                if(j>=currentColIndex){
-                                    $(this).val(columns[j-currentColIndex]);
+                var parts = clipboardData.split(/\s+/);
+                if(parts.length>0){
+                    tbody.find("tr").each(function(i){
+                        if(i>=currentRowIndex){
+                            if(typeof row[i-currentRowIndex]  !== 'undefined' && !row[i-currentRowIndex].isEmpty()){
+                                var columns = row[i-currentRowIndex].split(/\s+/);
+                                if(!params.pasteInCollapse){
+                                    $(this).find("td:not(.disable) input").each(function(j){
+                                        if(j>=currentColIndex && j<currentColIndex+columns.length){
+                                            $(this).val(columns[j-currentColIndex]);
+                                            $(this).trigger("change");
+                                        }
+                                    });
+                                }else{
+                                    $(this).find("td input").each(function(j){
+                                        if(j>=currentColIndex && j<currentColIndex+columns.length){
+                                            $(this).val(columns[j-currentColIndex]);
+                                            $(this).trigger("change");
+                                        }
+                                    });
                                 }
-                            });
+                            }
                         }
-                    }
-                });
+                    });
+                }   
             }, 10);
         });
     }
-    function enableCollapsableHead(ele) {
-        ele.click(function(){
+    function enableCollapsableHead(ele,table){
+        ele.off('clock');
+        ele.on('click',function(){
             var cele = $(this);
             var index = cele.data("index");
             var ocols = cele.data("ocols");
@@ -171,16 +201,16 @@ $.fn.foldableTable = function (t) {
                             }
                             if(cele.data("collapse")=="off"){
                                 mele.css("display","none");
+                                mele.addClass("disable");
                             }else{
                                 mele.css("display","table-cell");
+                                mele.removeClass("enable");
                             }
                         }
                     }
                 });
             }
         });
-
-
     }
     return this;
 };
@@ -190,6 +220,4 @@ $.fn.hasAttr = function(attr) {
 }; 
 String.prototype.isEmpty = function(text) {
     return (typeof text  !== 'undefined' && text.trim().length>0);
-    }; 
-
-    //plugin ended
+};
